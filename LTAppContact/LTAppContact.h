e9a0d02cf32c1588d2a3205c78c90de464bc09eb
Ofir Malachi <ofirmalachi@ofirs-macbook-pro.local>
2018-11-06 19:03:09 +0200

    Initial Commit

---
+++ LTAppContact/LTAppContact.h
@@ -0,0 +1,19 @@
+//
+//  LTAppContact.h
+//  LTAppContact
+//
+//  Created by Ofir Malachi on 06/11/2018.
+//  Copyright © 2018 www.LegoTechApps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LTAppContact.
+FOUNDATION_EXPORT double LTAppContactVersionNumber;
+
+//! Project version string for LTAppContact.
+FOUNDATION_EXPORT const unsigned char LTAppContactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTAppContact/PublicHeader.h>
+
+
